48583a7da3dca5128312111dda78b5ac82162c8b
Alex de Carvalho <107936326+AlexCMoreira10@users.noreply.github.com>
2026-05-20 12:51:24 -0300

    Teste Vercel

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import app from './src/app.js'; 
+// server.js (na raiz do projeto)
+import app from './src/app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`SERVIDOR RODANDO NA PORTA http://localhost:${PORT}`);
 });
-
